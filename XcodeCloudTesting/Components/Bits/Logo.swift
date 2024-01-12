45478e92599f9dc5747e8f2d50799310885a4f12
Daniel Hall <daniel@danielhall.io>
2024-01-11 21:34:56 -0700

    large

--- XcodeCloudTesting/Components/Bits/Logo.swift
+++ XcodeCloudTesting/Components/Bits/Logo.swift
@@ -10,6 +10,6 @@ import SwiftUI
 
 struct Logo: View {
     var body: some View {
-        Image(systemName: "hare.fill").imageScale(.medium)
+        Image(systemName: "hare.fill").imageScale(.large)
     }
 }
